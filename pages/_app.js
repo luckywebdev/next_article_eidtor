121fffebc9e0e7c8d8d792e5af61c30fc37eba04
Alex-Sasha-98 <sandrida.sasha@gmail.com>
2020-09-06 23:00:24 +0700

    app.js update

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import { SWRConfig } from 'swr'
 import fetch from '../lib/fetchJson'
 import '../static/css/article-editor.css';
-// import '../static/css/article-editor.min.css';
-// import '../static/css/arx-content.css';
-// import '../static/css/arx-frame.css';
-// import '../static/css/arx-frame.min.css';
-// import '../static/css/arx-content.min.css';
+import '../static/css/article-editor.min.css';
+import '../static/css/arx-content.css';
+import '../static/css/arx-frame.css';
+import '../static/css/arx-frame.min.css';
+import '../static/css/arx-content.min.css';
 
 
 function MyApp({ Component, pageProps }) {
